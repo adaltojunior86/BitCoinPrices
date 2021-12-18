33d72d58b4b20df49c1d52b79041402b71930f58
Adalto Junior <herdeiro.junior@gmail.com>
2021-12-18 11:10:26 -0300

    Adding files

--- app/src/main/java/com/adaltojunior/bitcoinprices/builder/RetrofitBuilder.kt
+++ app/src/main/java/com/adaltojunior/bitcoinprices/builder/RetrofitBuilder.kt
@@ -2,7 +2,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitBuilder {
-    private val URLBASE: String = "https://api.blockchain.info/";
+    private val URLBASE: String = "https://api.blockchain.info/"
     fun init(): Retrofit {
         return Retrofit.Builder().baseUrl(URLBASE).addConverterFactory(GsonConverterFactory.create()).build()
     }
